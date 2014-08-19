285c43cb9f6fab00715df07ca19be84902824568
Saneem <xaneem@gmail.com>
2014-08-19 14:54:44 +0530

    Changed generic field names

--- main.js
+++ main.js
@@ -3,41 +3,41 @@ window.ondragstart = function() { return false; }
 $(function() {
 
     chrome.storage.sync.get(function(item){
-        $('#username').val(item.username);
-        $('#password').val(item.password);
+        $('#nitc_username').val(item.username);
+        $('#nitc_password').val(item.password);
 
         if(item.last_error == 'true'){
-            $('#message').html("The last attempt failed due to invalid username/password.");
+            $('#nitc_message').html("The last attempt failed, probably due to an invalid username/password.");
         }
     });
 
     function saveChanges() {
-        var username = $('#username').val(),
-            password = $('#password').val();
+        var username = $('#nitc_username').val(),
+            password = $('#nitc_password').val();
 
         chrome.storage.sync.set({
             'username': username,
             'password': password,
         }, function() {
-            $('#submit').removeClass('submit').addClass('success');
-            $('#submit').html('SAVED');
+            $('#nitc_submit').removeClass('submit').addClass('success');
+            $('#nitc_submit').html('SAVED');
 
             setTimeout(function(){
-                $('#submit').removeClass('success').addClass('submit');
-                $('#submit').html('Save Credentials');
+                $('#nitc_submit').removeClass('success').addClass('submit');
+                $('#nitc_submit').html('Save Credentials');
             },1000);
         });
     }
 
 
-    $('#submit').on('click', function(event) {
+    $('#nitc_submit').on('click', function(event) {
         event.preventDefault();
         saveChanges();
 
         return false;
     });
 
-    $('#authenticate').on('click', function(event) {
+    $('#nitc_authenticate').on('click', function(event) {
         event.preventDefault();
         
         saveChanges();
